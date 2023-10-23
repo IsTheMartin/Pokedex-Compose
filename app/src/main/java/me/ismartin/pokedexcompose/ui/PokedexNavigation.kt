ced6b58056346146a5fc6e6a6badf8158b582517
Martin Morales <themartin93@gmail.com>
2023-10-23 12:08:08 -0600

    ktlintFormat

--- app/src/main/java/me/ismartin/pokedexcompose/ui/PokedexNavigation.kt
+++ app/src/main/java/me/ismartin/pokedexcompose/ui/PokedexNavigation.kt
@@ -1,5 +1,5 @@
 package me.ismartin.pokedexcompose.ui
 
 sealed class PokedexNavigation(val route: String) {
-    object PokedexScreen: PokedexNavigation("Pokedex")
+    object PokedexScreen : PokedexNavigation("Pokedex")
 }
